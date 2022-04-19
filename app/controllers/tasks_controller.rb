8df03be74cb04947764b3cfef24bc5018a66d4f7
sho.takanami <sho.takanami@gmail.com>
2022-04-19 11:30:08 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_message, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
     @tasks = Task.all
@@ -44,9 +44,9 @@ class TasksController < ApplicationController
     redirect_to tasks_url
   end
 
-private
+  private
 
-  def set_message
+  def set_task
     @task = Task.find(params[:id])
   end
   
